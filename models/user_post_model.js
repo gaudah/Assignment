389677ac0061240bbbdf9fc957072974d69418ee
Aishwarya Gaud <aishwarya.gaud@iauro.com>
2019-12-27 23:57:14 +0530

    user and post models created

---
+++ models/user_post_model.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    autoIncrement = require('mongoose-auto-increment')
+
+    userSchema = new Schema({
+        first_name: {type : String},
+        last_name: {type : String},
+        user_name: {type : String,unique:true},
+        email: {type : String,unique:true},
+        password: { type: String, default: null },
+        contact_no: [{type : Number}],
+        profile_image_url: {type : String},
+        location: { type: String, default: "" },
+        is_public:{type:Boolean, default:true}, // account type can be public or private
+        access_token: { type: String, default: null }, // i.e token
+        registered_on: { type: Date, default: new Date().toISOString() },
+        last_logged_in: { type: Date, default: new Date().toISOString() },
+    }),
+        commentSchema = new Schema({
+            comment_by : {type: String},
+            msg: {type:String},
+            created_date:{type: Date},
+            like: {type:Number}
+        }),
+    post = new Schema({
+        title: {type : String},
+            description: {type : String},
+        post_type: {type : String}, // i.e post type can be image,written blog etc.
+            post_by:userSchema,
+        url: {type : String},
+            tags: [{type : String}],
+        likes: {type:Number},
+        comment: [commentSchema],
+    },
+    { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
+    {strict: false });
+
+
+autoIncrement.initialize(mongoose.connection);
+
+post.plugin(autoIncrement.plugin, {
+    model: 'post',
+    field: 'id',
+    startAt: 1,
+    incrementBy: 1});
+
+const checkStrCapitalize = (str) => {
+    return str.match(/[A-Z]/);
+}
+
+
+post.methods.validTitle = function( title ) {
+    let result = "Invalid title entered"
+    if (checkStrCapitalize(title) === null) {
+        result = this.title;
+    }
+    return result;
+
+};
+
+
+exports.post = mongoose.model('post', post, 'post');
